c277fdcac53b4bda6a0cc4e0763fb4830bbd79bb
cycbill <cuiyechang1991@gmail.com>
2024-07-23 21:52:51 -0400

    Add BermudanSwaptionTest example

---
+++ Examples/BermudanSwaptionTest/BermudanSwaptionTest.cpp
@@ -0,0 +1,11 @@
+/*
+My Test Project of Bermudan Swaption pricing
+*/
+
+#include <ql/qldefines.hpp>
+#if !defined(BOOST_ALL_NO_LIB) && defined(BOOST_MSVC)
+#  include <ql/auto_link.hpp>
+#endif
+#include <ql/instruments/vanillaswap.hpp>
+#include <ql/instruments/swaption.hpp>
+
